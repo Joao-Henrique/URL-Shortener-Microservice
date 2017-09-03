4913ede53bd952dc33ec9aafca6b29450f15ee97
Joao Henrique <jppulicidade@gamail.com>
2017-09-03 21:41:55 +0100

    after 2 days of debugging... finaly it's done :)

--- keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: 'mongodb://johnypipe:123456@ds115124.mlab.com:15124/urlshortdb'
-};
--- server.js
+++ server.js
@@ -5,18 +5,29 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const ShortUrlModel = require ('./models/ShortUrl.model')
-const keys = require('./keys.js');
+const shortUrl = require ('./models/shortUrl')
 app.use(bodyParser.json());
 app.use(cors());
 // END /////////////////////////////////////////////
 ////////////////////////////////////////////////////
 
-mongoose.connect(keys.mongoURI);
 
 ////////////////////////////////////////////////////
 //ALLOWS NODE TO FIND STATIC CONTENT (HTML AND CSS)/
 app.use(express.static(__dirname + '/public'));
+// END /////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+
+////////////////////////////////////////////////////
+////////////   CONNECT TO DATABASE   ///////////////
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://admin:admin@ds115124.mlab.com:15124/urlshortdb')
+var db = mongoose.connection
+db.on('error',console.error.bind(console,'connection error:'))
+db.once('open', ()=> {
+    console.log('Succesfuly connected to DB')
+})              
 // END /////////////////////////////////////////////
 ////////////////////////////////////////////////////
 
@@ -28,14 +39,16 @@ app.get('/new/:urlToShorten(*)', (req, res) => {
   var regEx = /[-a-zA-Z0-9@:%\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%\+.~#?&//=]*)?/gi;
   if (regEx.test(urlToShorten)===true) {
     var short = Math.floor(Math.random()*100000).toString();
-    var data = new ShortUrlModel({
+    var data = new shortUrl({
       originalUrl: urlToShorten,
-      shorterUrl: 'localhost:' + portNumber + '/' + short
+      shorterUrl: short
     });
-    data.save();
+    data.save((err, data) => {
+      if (err) return console.error(err)
+    });
     return res.json(data)
   }
-  var data = new ShortUrlModel({
+  var data = new shortUrl({
     originalUrl: urlToShorten,
     shorterUrl: 'Your URL is invalid'
   })
@@ -49,9 +62,10 @@ app.get('/new/:urlToShorten(*)', (req, res) => {
 ////////////   QUERY DB AND GO TO URL   ////////////
 app.get('/:urlToForward', (req, res) => {
   var shorterUrl = req.params.urlToForward;
-  ShortUrlModel.findOne({'shorterUrl': shorterUrl}, (err, data) => {
+  shortUrl.findOne({'shorterUrl': shorterUrl}, (err, data) => {
     if (err) return res.send('Error reading database');
     var re = new RegExp("^(http|https)://", "i");
+    console.log(data.originalUrl)
     var strToCheck = data.originalUrl;
     if(re.test(strToCheck)){
       res.redirect(301, data.originalUrl);
@@ -67,8 +81,8 @@ app.get('/:urlToForward', (req, res) => {
 
 ////////////////////////////////////////////////////
 ////////////   LISTEN ON PORT   ////////////////////
-const portNumber = 3000;
-app.listen(process.env.port || portNumber, () => 
-  console.log('App Listening on Port ' + portNumber + '...'));
+const portNumber = (process.env.port) || 3000;
+app.listen( portNumber, () => 
+  console.log('YOUR SERVER IS RUNNING...'));
 // END /////////////////////////////////////////////
 ////////////////////////////////////////////////////
